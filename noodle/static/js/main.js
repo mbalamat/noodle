2e0e22a1e0f96ddad2840a4c192a7c35b545addd
Marios Balamatsias <mbalamatsias@gmail.com>
2016-09-23 23:31:59 +0300

    Add time picker

--- noodle/static/js/main.js
+++ noodle/static/js/main.js
@@ -15,7 +15,34 @@ $(document).ready(function() {
             if (dates_selected.length != 0) {
                 var selectedDatesHTML = "";
                 for (i = 0; i < dates_selected.length; i++) {
-                    selectedDatesHTML += "<ul>".concat(dates_selected[i]).concat("</ul>");
+                    var ddmmyyyy = dates_selected[i].getDate() + "/" + (dates_selected[i].getMonth() + 1) + "/" + dates_selected[i].getFullYear();
+                    var timeSelector = '<select multiple class="form-control" id="sel2" style="margin-left:7em; width: 30%;"> \
+                        <option>1</option> \
+                        <option>2</option> \
+                        <option>3</option> \
+                        <option>4</option> \
+                        <option>5</option> \
+                        <option>6</option> \
+                        <option>7</option> \
+                        <option>8</option> \
+                        <option>9</option> \
+                        <option>10</option> \
+                        <option>11</option> \
+                        <option>12</option> \
+                        <option>13</option> \
+                        <option>14</option> \
+                        <option>15</option> \
+                        <option>16</option> \
+                        <option>17</option> \
+                        <option>18</option> \
+                        <option>19</option> \
+                        <option>20</option> \
+                        <option>21</option> \
+                        <option>22</option> \
+                        <option>23</option> \
+                        <option>24</option> \
+                        </select>';
+                    selectedDatesHTML += '<li class="list-group-item">' + ddmmyyyy + timeSelector  + "</li>";
                 }
                 document.getElementById("timedates").innerHTML = selectedDatesHTML;
             }
